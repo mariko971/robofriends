f57647d7365315285347d18e51e157ec5d719db4
Mark Njuru <66862952+mariko971@users.noreply.github.com>
2024-11-27 11:56:03 +0400

    connected the app.js component to redux and in index.js created the redux store and applied middlewares.

--- src/index.js
+++ src/index.js
@@ -1,16 +1,30 @@
-import React from 'react';
+import React from "react";
+import { Provider } from "react-redux";
+import {
+  legacy_createStore as createStore,
+  applyMiddleware,
+  combineReducers,
+} from "redux";
+import { createLogger } from "redux-logger";
+import { thunk } from "redux-thunk";
 // import ReactDOM from 'react-dom'; The new way to import createRoot:
 import { createRoot } from "react-dom/client";
-import './index.css'; 
-import App from './containers/App';
-import 'tachyons';
+import "./index.css";
+import App from "./containers/App";
+import "tachyons";
 // import registerServiceWorker from './registerServiceWorker';
+import { searchRobots, requestRobots } from "./reducers";
 
+const rootReducer = combineReducers({ searchRobots, requestRobots });
+const logger = createLogger();
+const store = createStore(rootReducer, applyMiddleware(thunk, logger));
 
-
-const root = createRoot(document.getElementById('root'));
-root.render(<App />);
-
+const root = createRoot(document.getElementById("root"));
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
 
 // ReactDOM.render(<App />, document.getElementById('root'));
 // registerServiceWorker();
